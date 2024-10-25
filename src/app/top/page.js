d437a34770390d1393ee652f571c8a62b3a66de6
fukugit <fukuma1234@gmail.com>
2024-10-25 17:45:27 +0900

    Add login page

--- src/app/top/page.js
+++ src/app/top/page.js
@@ -1,10 +1,23 @@
+'use client'
 import Link from "next/link";
-import styles from "./page.module.css";
+import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
+  // Get token from URL query string.
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
   return (
-    <div className={styles.page}>
-      <h1>This is main page.</h1>
+    <div>
+      {token ? ( // message が存在する場合のみ表示
+        <div className="flex justify-center items-center h-screen w-full bg-emerald-300">
+          <p className="w-48">token: {token}</p>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-screen w-full bg-amber-300">
+          <p className="w-48">API doens't work...</p>
+        </div>
+      )}
     </div>
   );
 }
