eb35e0b22e38f143631795dcaddd6bc9c0e3c70b
fukugit <fukuma1234@gmail.com>
2024-10-27 18:56:57 +0900

    Add Sidebar

---
+++ src/components/app-sidebar.jsx
@@ -0,0 +1,74 @@
+import * as React from "react"
+import Link from "next/link";
+
+import { VersionSwitcher } from "@/components/version-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+  navMain: [
+    {
+      title: "",
+      url: "#",
+      items: [
+        {
+          title: "ホーム",
+          url: "/top",
+        },
+        {
+          title: "猫保有情報",
+          url: "/registration",
+        },
+        {
+          title: "ログアウト",
+          url: "/",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({
+  ...props
+}) {
+  return (
+    (<Sidebar {...props}>
+      <SidebarHeader>
+        <VersionSwitcher versions={data.versions} defaultVersion={data.versions[0]} />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    {/* <SidebarMenuButton asChild isActive={item.isActive}>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton> */}
+                    <Link href={item.url}>{item.title}</Link>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>)
+  );
+}
