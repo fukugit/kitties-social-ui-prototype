a7690f495d11b68e318cd8832cc9da2f01560181
kifumi01 <aki.suzu.1126@gmail.com>
2024-11-03 15:22:15 +0900

    猫の血統書画像のアップロード機能を実装

--- src/app/registration/enter/page.js
+++ src/app/registration/enter/page.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { useRouter } from 'next/navigation';
 import axios from "axios";
 import { Button } from "@/components/ui/button"
+import { useDropzone } from 'react-dropzone';
 
 export default function Registration() {
   const router = useRouter()
@@ -12,14 +13,25 @@ export default function Registration() {
   const nextpage = () => {
     router.push('/registration/confirmation')
   }
+
+  const onDrop = (acceptedFiles) => {
+    console.log(acceptedFiles);
+  }
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
     <>
       <div className="container flex justify-center h-screen w-full mt-10">
-        <div className="w-80 flex gap-2 flex-col items-center">
-          <p className="text-2xl">猫情報登録</p>
+        <div className="w-100 flex gap-2 flex-col items-center">
+          <p className="text-2xl flex justify-start">猫情報登録</p>
           <p className="mb-10">
-            xxxxxxxxxxxxx<br/>
+            血統書の情報から親猫情報を登録します。登録する猫の頭数分の血統書をアップロードしてください。
           </p>
+          <div {...getRootProps()} className="border-2 border-dashed border-gray-400 p-4 text-center">
+            <input {...getInputProps()} />
+            <p>クリックしてアップロード またはドラッグ＆ドロップしてください</p>
+          </div>
           <Button className="w-full bg-blue-800 hover:bg-blue-900" size="lg" variant="default"
             onClick={nextpage}>次へ
           </Button>
